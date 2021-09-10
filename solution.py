cbbdb69f588934a5495b5e69e632a2ddeb27e0f1
sam <sdm8472@nyu.edu>
2021-09-10 11:21:34 -0400

    First commit of solution.py

--- solution.py
+++ solution.py
@@ -12,7 +12,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "hey test"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "hey test2"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "The student should type the answer here"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
